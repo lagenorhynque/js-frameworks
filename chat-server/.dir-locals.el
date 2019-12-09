6994d1071acdd9edb28c763b6bb4a31212e66925
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-12-09 22:00:25 +0900

    Ignore .dir-local.el

--- chat-server/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((cider-refresh-before-fn . "integrant.repl/suspend")
-         (cider-refresh-after-fn  . "integrant.repl/resume")))
- (clojure-mode . ((eval . (define-clojure-indent
-                            ;; test.check
-                            (for-all :defn))))))
